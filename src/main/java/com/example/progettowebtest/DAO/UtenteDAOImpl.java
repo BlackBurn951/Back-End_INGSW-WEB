e93639a5dab5f51c78bed857df40d175cb22e8c2
Luke6933 <secondary.bonanno@gmail.com>
2024-01-04 21:43:44 +0100

    Completato invio email_OTP

--- src/main/java/com/example/progettowebtest/DAO/UtenteDAOImpl.java
+++ src/main/java/com/example/progettowebtest/DAO/UtenteDAOImpl.java
@@ -22,14 +22,17 @@ public class UtenteDAOImpl implements UtenteDAO {
     public void saveOrUpdate(Utente ut) {
         try {
             PreparedStatement statement;
-            String query= "insert into utente (cf, nome, cognome, cittadinanza, comune_di_nascita, sesso, provincia_di_nascita, num_telefono, data_di_nascita, " +
+            String query = "insert into utente (cf, nome, cognome, cittadinanza, comune_di_nascita, sesso, provincia_di_nascita, num_telefono, data_di_nascita, " +
                     "email, password, num_identificativo_ci, num_patente, num_passaporto, nome_via_domicilio, num_civico_domicilio, nome_via_residenza, " +
                     "num_civico_residenza, id_comune_residenza, id_comune_domicilio, id_via_residenza, id_via_domicilio, occupazione, reditto_annuo) " +
                     "values(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
 
-        }catch (SQLException e) {
-            e.printStackTrace();
+        } finally {
+
         }
+        /*catch (SQLException e) {
+            e.printStackTrace();
+        }*/
     }
 
     @Override
