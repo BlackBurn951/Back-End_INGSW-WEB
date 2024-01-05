9a74ea6a6ef65f72f111053c21b71ecdf2329b09
BlackBurn951 <aldonocera95@gmail.com>
2024-01-06 00:05:01 +0100

    Creazione funzione checkEmail e dichiaraizione metodo per il controllo dell'OTP

---
+++ src/main/java/com/example/progettowebtest/ClassiRequest/DatiAnagrafici.java
@@ -0,0 +1,96 @@
+package com.example.progettowebtest.ClassiRequest;
+
+public class DatiAnagrafici {
+    private String nome;
+    private String cognome;
+    private String cellulare;
+    private String email;
+    private String cf;
+    private String dataNascita;
+    private String sesso;
+    private String comune;
+    private String prov;
+    private String cittadinanza;
+
+    public DatiAnagrafici() {}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public String getCellulare() {
+        return cellulare;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCf() {
+        return cf;
+    }
+
+    public String getDataNascita() {
+        return dataNascita;
+    }
+
+    public String getSesso() {
+        return sesso;
+    }
+
+    public String getComune() {
+        return comune;
+    }
+
+    public String getProv() {
+        return prov;
+    }
+
+    public String getCittadinanza() {
+        return cittadinanza;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public void setCellulare(String cellulare) {
+        this.cellulare = cellulare;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setCf(String cf) {
+        this.cf = cf;
+    }
+
+    public void setDataNascita(String dataNascita) {
+        this.dataNascita = dataNascita;
+    }
+
+    public void setSesso(String sesso) {
+        this.sesso = sesso;
+    }
+
+    public void setComune(String comune) {
+        this.comune = comune;
+    }
+
+    public void setProv(String prov) {
+        this.prov = prov;
+    }
+
+    public void setCittadinanza(String cittadinanza) {
+        this.cittadinanza = cittadinanza;
+    }
+}
