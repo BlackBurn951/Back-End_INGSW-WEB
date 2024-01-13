bd1110697f8806fd3277e38256915b77f3c6505c
Aldo Nocera <131450373+BlackBurn951@users.noreply.github.com>
2024-01-13 15:28:00 +0100

    Update SendEmailController.java

--- src/main/java/com/example/progettowebtest/EmailSender/SendEmailController.java
+++ src/main/java/com/example/progettowebtest/EmailSender/SendEmailController.java
@@ -2,7 +2,8 @@ package com.example.progettowebtest.EmailSender;
 import com.google.api.services.gmail.model.Message;
 import java.io.*;
 import java.security.GeneralSecurityException;
-import javax.mail.MessagingException;
+import jakarta.mail.MessagingException;
+import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
@@ -12,48 +13,42 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-
 import static com.example.progettowebtest.EmailSender.EmailService.*;
 import static com.example.progettowebtest.EmailSender.OTPGenerator.generateOTP;
 
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
-public class SendEmailController{
+public class SendEmailController {
     public static String generatedOTP;
 
-
     @PostMapping("/sendEmail")
-    public void sendEmail(HttpSession session, @RequestBody EmailData emailData) {
-        String nomeCognome = emailData.getNomeCognome();
+    public void sendEmail(HttpServletRequest request, @RequestBody EmailData extendedEmailData) {
+        String nomeCognome = extendedEmailData.getNomeCognome();
 
 
         try {
-            if(emailData.isAllegato()){
+            if(extendedEmailData.isAllegato()){
                 String emailTemplate = EmailTemplateLoader.loadEmailTemplate("/email_pdf_template.html");
                 String pdf_html = emailTemplate
-                        .replace("EMAIL_DESTINATARIO", emailData.getTo())
-                        .replace("NOME_COGNOME", emailData.getNomeCognome())
-                        .replace("DATA_NASCITA", emailData.getDataDiNascita())
-                        .replace("INDIRIZZO", emailData.getIndirizzo())
-                        .replace("NUMERO_TELEFONO", emailData.getNumeroTelefono())
-                        .replace("DATA_FIRMA", emailData.getDataFirma());
+                        .replace("EMAIL_DESTINATARIO", extendedEmailData.getTo())
+                        .replace("NOME_COGNOME", extendedEmailData.getNomeCognome())
+                        .replace("DATA_NASCITA", extendedEmailData.getDataDiNascita())
+                        .replace("INDIRIZZO", extendedEmailData.getIndirizzo())
+                        .replace("NUMERO_TELEFONO", extendedEmailData.getNumeroTelefono())
+                        .replace("DATA_FIRMA", extendedEmailData.getDataFirma());
 
-                PDDocument pdfFile = CreaPDFConfermaConto.creaPDFconto(emailData.getNomeCognome(), emailData.getDataDiNascita(), emailData.getIndirizzo(), emailData.getNumeroTelefono(),emailData.getTo(), emailData.getDataFirma());
-                Message message = EmailService.createMessageWithAttachment(pdf_html, emailData.getSender(), emailData.getTo(), emailData.getSubject(), pdfFile);
-                sendMessage(getService(), emailData.getUserId(), message);
+                PDDocument pdfFile = CreaPDFConfermaConto.creaPDFconto(extendedEmailData.getNomeCognome(), extendedEmailData.getDataDiNascita(), extendedEmailData.getIndirizzo(), extendedEmailData.getNumeroTelefono(),extendedEmailData.getTo(), extendedEmailData.getDataFirma());
+                Message message = EmailService.createMessageWithAttachment(pdf_html, extendedEmailData.getSender(), extendedEmailData.getTo(), extendedEmailData.getSubject(), pdfFile);
+                sendMessage(getService(), extendedEmailData.getUserId(), message);
 
 
             }else{
                 //Generazione e ripristino sessione
+                HttpSession session= request.getSession(false);
                 if(session!=null)
                     session.invalidate();
-
-                HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-                session = request.getSession(true);
-
+                session= request.getSession(true);
 
                 //Generazione otp e assegnamento alla sessione
                 String generatedOTP = generateOTP();;
@@ -63,16 +58,14 @@ public class SendEmailController{
                 String htm_otp = emailTemplate
                         .replace("$NOME_COGNOME$", nomeCognome)
                         .replace("$GENERATED_OTP$", generatedOTP);
-                Message message = createMessage(emailData.getSender(), emailData.getTo(), emailData.getSubject(), htm_otp);
-                sendMessage(getService(), emailData.getUserId(), message);
+                Message message = createMessage(extendedEmailData.getSender(), extendedEmailData.getTo(), extendedEmailData.getSubject(), htm_otp);
+                sendMessage(getService(), extendedEmailData.getUserId(), message);
 
 
             }
 
-        } catch (IOException | GeneralSecurityException e) {
+        } catch (IOException | GeneralSecurityException | MessagingException e) {
             e.printStackTrace();
-            throw new RuntimeException(e);
-        } catch (MessagingException e) {
             throw new RuntimeException(e);
         }
     }
