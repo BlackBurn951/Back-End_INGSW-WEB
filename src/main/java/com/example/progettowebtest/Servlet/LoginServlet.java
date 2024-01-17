a0055c315b593808db8af307b6592830706ab59d
Luke6933 <127422711+Luke6933@users.noreply.github.com>
2024-01-17 01:17:56 +0100

    Changes by aldon

--- src/main/java/com/example/progettowebtest/Servlet/LoginServlet.java
+++ src/main/java/com/example/progettowebtest/Servlet/LoginServlet.java
@@ -1,17 +1,16 @@
 package com.example.progettowebtest.Servlet;
 
+import com.example.progettowebtest.ClassiRequest.CambioPassword;
 import com.example.progettowebtest.ClassiRequest.IdentificativiUtente;
 import com.example.progettowebtest.DAO.MagnusDAO;
 import com.example.progettowebtest.Model.Utente_Documenti.Utente;
 import org.springframework.security.crypto.bcrypt.BCrypt;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200", exposedHeaders = "Session-ID")
 public class LoginServlet {
+
     @GetMapping("/login")
     public boolean doLogin(@RequestParam("username") String username, @RequestParam("password") String password) {
         boolean result= false;
@@ -21,5 +20,26 @@ public class LoginServlet {
             result= true;
 
         return result;
+    }
+
+    @GetMapping("/checkUser")
+    public boolean checkUser(@RequestParam("username") String username) {
+        boolean result= false;
+
+        Utente ut= MagnusDAO.getInstance().getUtenteDAO().doRetriveByKey(username, IdentificativiUtente.EMAIL);
+        if(ut!=null)
+            result= true;
+
+        return result;
+    }
+
+    @PostMapping("/recuperaPass")
+    public void recuperaPass(@RequestBody CambioPassword cambio) {
+        System.out.println("password nuova arrivata: "+cambio.getPassword());
+        Utente ut= MagnusDAO.getInstance().getUtenteDAO().doRetriveByKey(cambio.getEmail(), IdentificativiUtente.EMAIL);
+        System.out.println("Vecchia password criptata: "+ut.getPassword());
+        ut.setPassword(cambio.getPassword());
+        MagnusDAO.getInstance().getUtenteDAO().saveOrUpdate(ut);
+        System.out.println("Nuova password criptata: "+ut.getPassword());
     }
 }
