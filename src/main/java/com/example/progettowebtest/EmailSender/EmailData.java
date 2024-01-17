4095af89d7694669d95d78817b8501546fcc736f
Luke6933 <127422711+Luke6933@users.noreply.github.com>
2024-01-17 03:03:02 +0100

    Aggiornato tempo sessione

--- src/main/java/com/example/progettowebtest/EmailSender/EmailData.java
+++ src/main/java/com/example/progettowebtest/EmailSender/EmailData.java
@@ -6,36 +6,6 @@ public class EmailData {
     private String to;
     private String subject;
     private String userId;
-    private boolean allegato;
-    private String dataDiNascita;
-    private String indirizzo;
-    private String numeroTelefono;
-    private String dataFirma;
-
-    public String getDataFirma() {
-        return dataFirma;
-    }
-
-    public String getDataDiNascita() {
-        return dataDiNascita;
-    }
-
-    public String getIndirizzo() {
-        return indirizzo;
-    }
-
-    public void setIndirizzo(String indirizzo) {
-        this.indirizzo = indirizzo;
-    }
-
-    public String getNumeroTelefono() {
-        return numeroTelefono;
-    }
-
-    public boolean isAllegato() {
-        return allegato;
-    }
-
 
     public String getNomeCognome() {
         return nomeCognome;
@@ -44,7 +14,6 @@ public class EmailData {
     public String getTo() {
         return to;
     }
-
 
     public String getSubject() {
         return subject;
--- src/main/java/com/example/progettowebtest/Servlet/RegistrazioneServlet.java
+++ src/main/java/com/example/progettowebtest/Servlet/RegistrazioneServlet.java
@@ -10,17 +10,29 @@ import com.example.progettowebtest.DAO.StatoDAO;
 import com.example.progettowebtest.DAO.StatoDAOImpl;
 import com.example.progettowebtest.DAO.Utente_Documenti.*;
 import com.example.progettowebtest.ClassiRequest.IdentificativiUtente;
+import com.example.progettowebtest.EmailSender.CreaPDFConfermaConto;
+import com.example.progettowebtest.EmailSender.EmailService;
+import com.example.progettowebtest.EmailSender.EmailTemplateLoader;
 import com.example.progettowebtest.Model.ContoCorrente.ContoCorrente;
 import com.example.progettowebtest.Model.Indirizzo.ColonneDatiComune;
 import com.example.progettowebtest.Model.Indirizzo.DatiComune;
 import com.example.progettowebtest.Model.Indirizzo.Indirizzo;
 import com.example.progettowebtest.Model.Indirizzo.TipoVia;
 import com.example.progettowebtest.Model.Utente_Documenti.*;
+import com.google.api.services.gmail.model.Message;
+import jakarta.mail.MessagingException;
 import jakarta.servlet.http.*;
+import org.apache.pdfbox.pdmodel.PDDocument;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.time.LocalDate;
 import java.util.Vector;
+import java.util.concurrent.TimeUnit;
+
+import static com.example.progettowebtest.EmailSender.EmailService.getService;
+import static com.example.progettowebtest.EmailSender.EmailService.sendMessage;
 
 
 @RestController
