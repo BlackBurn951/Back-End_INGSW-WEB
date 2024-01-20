1c449804bb7ca477cc60e4a28ce0b2c4415c5055
BlackBurn951 <aldonocera95@gmail.com>
2024-01-20 15:35:44 +0100

    Modifiche per servlet carte

--- src/main/java/com/example/progettowebtest/Servlet/CarteServlet.java
+++ src/main/java/com/example/progettowebtest/Servlet/CarteServlet.java
@@ -1,31 +1,12 @@
 package com.example.progettowebtest.Servlet;
 
-import com.example.progettowebtest.ClassiRequest.DatiCarta;
-import com.example.progettowebtest.DAO.MagnusDAO;
-import com.example.progettowebtest.EmailSender.EmailData;
-import com.example.progettowebtest.EmailSender.EmailTemplateLoader;
-import com.example.progettowebtest.EmailSender.SendEmailController;
-import com.example.progettowebtest.Model.Carte.CartaProxy;
-import com.example.progettowebtest.Model.Carte.Carte;
-import com.example.progettowebtest.Model.Carte.TipiCarte;
 import com.example.progettowebtest.Model.ContoCorrente.ContoCorrente;
 import com.example.progettowebtest.Model.Utente_Documenti.Utente;
-import com.example.progettowebtest.Model.ValoriStato;
-import com.google.api.services.gmail.model.Message;
-import jakarta.mail.MessagingException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.time.LocalDate;
-import java.util.Date;
-import java.util.Objects;
-import java.util.concurrent.TimeUnit;
-
-import static com.example.progettowebtest.EmailSender.EmailService.*;
 import static com.example.progettowebtest.EmailSender.OTPGenerator.generateOTP;
 
 @RestController
@@ -33,13 +14,13 @@ import static com.example.progettowebtest.EmailSender.OTPGenerator.generateOTP;
 public class CarteServlet {
 
     @PostMapping("/creaCarta")
-    public boolean creaCarta(HttpServletRequest request, HttpServletResponse response, @RequestParam("IDSession") String idSession, @RequestBody DatiCarta dati) {
+    public boolean creaCarta(HttpServletRequest request, HttpServletResponse response, @RequestParam("IDSession") String idSession) {
         HttpSession session = (HttpSession) request.getServletContext().getAttribute(idSession);
         Utente ut = (Utente) session.getAttribute("Utente");
         ContoCorrente cc = (ContoCorrente) session.getAttribute("Conto");
         String generatedOTP = generateOTP();
 
-
+        /*
         double canone= 0.0, fido= 0.0;
         TipiCarte tipo;
         if(dati.getCredito()) {
@@ -52,6 +33,8 @@ public class CarteServlet {
             tipo= TipiCarte.DEBITO;
         }
         Carte carta = new CartaProxy(dati.getNumeroCarta(), true, LocalDate.now().toString(), dati.getDataScadenza(), dati.getCvv(), false, canone, generatedOTP, fido, MagnusDAO.getInstance().getStatoDAO().doRetriveByAttribute(ValoriStato.ATTIVO), tipo);
+        */
         return true;
+
     }
 }
