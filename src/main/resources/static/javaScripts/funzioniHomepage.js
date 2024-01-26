7d2dab1548ce1a8f7f34dc86f2a52ae37af107c6
Luke6933 <127422711+Luke6933@users.noreply.github.com>
2024-01-26 14:49:19 +0100

    aggiunti filtri transazioni(Con rima)

--- src/main/resources/static/javaScripts/funzioniHomepage.js
+++ src/main/resources/static/javaScripts/funzioniHomepage.js
@@ -207,3 +207,80 @@ function eliminaNotifica(element) {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function searchTransactions() {
+    var input = document.getElementById("searchInput");
+    var filter = input.value.toUpperCase();
+
+    var table = document.getElementById("transazioniTable");
+    var tbody = table.getElementsByTagName("tbody")[0];
+
+    var rows = tbody.getElementsByTagName("tr");
+
+    for (var i = 0; i < rows.length; i++) {
+        var transactionData = rows[i].getElementsByTagName("td");
+        var shouldDisplay = false;
+
+        for (var j = 0; j < transactionData.length; j++) {
+            var data = transactionData[j].textContent || transactionData[j].innerText;
+            if (data.toUpperCase().indexOf(filter) > -1) {
+                shouldDisplay = true;
+                break;
+            }
+        }
+        if (shouldDisplay) {
+            rows[i].style.display = "";
+        } else {
+            rows[i].style.display = "none";
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const transactionTipo = urlParams.get("tipoTrans");
+
+    if (transactionTipo) {
+        sessionStorage.setItem('selectedTransactionType', transactionTipo);
+    }
+});
+
+
+function filterTransactions() {
+    var select = document.getElementById("filterSelect");
+    var filterValue = select.value.toUpperCase();
+
+    var table = document.getElementById("transazioniTable");
+    var tbody = table.getElementsByTagName("tbody")[0];
+
+    var rows = tbody.getElementsByTagName("tr");
+
+    for (var i = 0; i < rows.length; i++) {
+        var transactionType = rows[i].getElementsByTagName("td")[0].textContent.trim().toUpperCase();
+
+        if (filterValue === "ALL" || transactionType === filterValue) {
+            rows[i].style.display = "";
+        } else {
+            rows[i].style.display = "none";
+        }
+    }
+}
+
+
+
